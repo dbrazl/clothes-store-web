0fe073a8fec5fdc8e1a559c3b0c6aef8d5dac441
Daniel Braz <danielbrazlopes16@gmail.com>
2023-04-28 18:13:10 -0300

    feat: create new test to home

--- src/pages/Home/home.test.tsx
+++ src/pages/Home/home.test.tsx
@@ -1,21 +1,43 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
 import Home from '.'
 
 test('should have a top bar', () => {
-  render(<Home />)
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  )
   const topBar = screen.getByTestId('top-bar')
   expect(topBar).toBeInTheDocument()
 })
 
 test('should have a header', () => {
-  render(<Home />)
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  )
   const header = screen.getByTestId('header')
   expect(header).toBeInTheDocument()
 })
 
 test('should have a carousel', () => {
-  render(<Home />)
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  )
   const carouselItem = screen.getAllByTestId('carousel-item')
   expect(carouselItem.length).toBeGreaterThan(0)
+})
+
+test('should have the home title on page', async () => {
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  )
+  await waitFor(() => { expect(document.title).toEqual('Magasin') })
 })
