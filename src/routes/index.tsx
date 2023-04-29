510638c47484e207ce25d7dcb40f7486d8b0a575
Daniel Braz <danielbrazlopes16@gmail.com>
2023-04-28 21:23:02 -0300

    feat: scroll to top when change route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import { Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Routes, useLocation } from 'react-router-dom'
 
 import publicRoutes from './PublicRoutes'
 import privateRoutes from './PrivateRoutes'
 import wildcardRoutes from './WildcardRoutes'
 
 export function AppRoutes (): JSX.Element {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <Routes>
       {publicRoutes()}
