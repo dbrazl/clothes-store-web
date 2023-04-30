096720d4de269839fa5ac3e9216566d9a2fa63b2
Daniel Braz <danielbrazlopes16@gmail.com>
2023-04-30 15:06:06 -0300

    fix: height of button

--- src/components/Button/styles.tsx
+++ src/components/Button/styles.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.button`
   width: 100%;
   height: 50px;
+  min-height: 50px;
   color: white;
   background-color: black;
   text-transform: uppercase;
